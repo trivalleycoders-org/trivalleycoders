fb5fe2ed44050f01868fdad9d50ee7471ebfe29f
klequis <klequis@gmail.com>
2017-05-08 13:19:26 -0700

    mongodb added, events being retrived form mongodb

---
+++ server/scripts/init.mongo.js
@@ -0,0 +1,29 @@
+db = new Mongo().getDB('tvcwebsite');
+db.events.remove({});
+db.events.insert([
+  {
+    id: 1,
+    date: new Date('2017-05-06'),
+    name: 'Extreme JavaScript Jam Session',
+    location: "Specialty's Cafe, Dublin, CA",
+  },
+  {
+    id: 2,
+    date: new Date('2017-05-07'),
+    name: 'Extreme JavaScript Jam Session',
+    location: "Specialty's Cafe, Dublin, CA",
+  },
+  {
+    id: 3,
+    date: new Date('2017-05-08'),
+    name: 'Extreme JavaScript Jam Session',
+    location: "Specialty's Cafe, Dublin, CA",
+  },
+  {
+    id: 4,
+    date: new Date('2017-05-09'),
+    name: 'Extreme JavaScript Jam Session',
+    location: "Specialty's Cafe, Dublin, CA",
+  },
+]);
+db.events.createIndex({ date: 1 });
--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
+import SourceMapSupport from 'source-map-support';
+SourceMapSupport.install();
+// import 'babel-polyfill';
 import bodyParser from 'body-parser'
 import express from 'express'
 import path from 'path'
-import events from './seed-data/events'
-
+// import events from './seed-data/events'
+import { MongoClient } from 'mongodb';
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+let db;
 
 const router = express.Router()
 
@@ -14,6 +19,10 @@ const staticFiles = express.static(path.join(__dirname, '../../client/build'))
 
 app.use(staticFiles)
 
+/*
+    This is for testing and not part of the application
+    Pending deletion
+ */
 router.get('/cities', (req, res) => {
   const cities = [
     {name: 'New York City', population: 8175133},
@@ -24,8 +33,17 @@ router.get('/cities', (req, res) => {
 })
 
 router.get('/events', (req, res) => {
-  const e = events
-  res.send(e)
+  db.collection('events').find().toArray()
+    .then(events => {
+      const metadata = { total_count: events.length}
+      res.json(events)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: 'Internal Server Error' })
+    })
+  // const e = events
+  // res.send(e)
 })
 
 app.use(router)
@@ -36,3 +54,13 @@ app.set('port', (process.env.PORT || 3001))
 app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`)
 })
+
+
+MongoClient.connect('mongodb://localhost/tvcwebsite').then(connection => {
+  db = connection;
+  app.listen(3002, () => {
+    console.log('App started on port 3000');
+  });
+}).catch(error => {
+  console.log('ERROR:', error);
+});
