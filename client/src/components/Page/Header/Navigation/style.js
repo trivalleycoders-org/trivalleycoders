3cb37552708efca1d0b81de7a5c449322a1af743
klequis <klequis@gmail.com>
2017-05-19 21:28:56 -0700

    missed file

--- client/src/components/Page/Header/Navigation/style.js
+++ client/src/components/Page/Header/Navigation/style.js
@@ -6,18 +6,3 @@ export const wrapper = {
   // alignItems: 'center',
   // alignContent: 'center',
 };
-
-export const navBtn = {
-  /*
-  backgroundColor: 'Transparent',
-  // color: 'white',
-  border: '1px solid #58565a',
-  fontFamily: '"titillium", sans-serif',
-  width: 90,
-  height: 30,
-  lineHeight: '24px',
-  padding: '0 5px 0 5px',
-  marginRight: 20,
-  letterSpacing: 2,
-  */
-}
