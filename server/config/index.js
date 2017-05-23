aa29a1ab486724ea42c1af3ed96ad8b2add38013
Chad C <chad187@gmail.com>
2017-05-23 09:54:30 -0700

    bypassing local db

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 var _ = require("lodash");
 var defaults = require("./default.js");
-var config = require("./" + (process.env.NODE_ENV || "development") + ".js");
-// var config = require("./production.js"); // a test for production
+// var config = require("./" + (process.env.NODE_ENV || "development") + ".js");//local database
+var config = require("./testing.js"); //online database
 module.exports = _.merge({}, defaults, config);
