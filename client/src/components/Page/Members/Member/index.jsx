9a7596be426892585b390c175ca4a0d9ee5cb212
klequis <klequis@gmail.com>
2017-05-29 09:30:44 -0700

    members styling good enough. Sponsors done same as Members but needs to be tweaked. Removed device from media queries

--- client/src/components/Page/Members/Member/index.jsx
+++ client/src/components/Page/Members/Member/index.jsx
@@ -4,14 +4,14 @@ import { Panel, Image } from 'react-bootstrap'
 import * as style from './style.css';
 
 const Member = (props) => (
-  <div id='member-panel' className='panel-member blue-background'>
+  <Panel id='panel-member' className='panel-member'>
     {/* className='img-sizing' */}
-    <div id='img-container' className='panel-member-img-container red-background'>
-      <Image src={props.picture} responsive alt='member icon' />
+    <div id='member-img-container' className='member-img-container red-background'>
+      <Image src={props.picture} responsive alt='member picture' />
     </div>
     <h4 id='member-name' className='orange-background'>{props.name}</h4>
-    <h5 id='member-role' className='orange-background'>{props.role}</h5>
-  </div>
+    <h5 id='member-role' className='member-role orange-background'>{props.role}</h5>
+  </Panel>
 );
 
 export default Member;
--- client/src/components/Page/Sponsors/Sponsor/index.jsx
+++ client/src/components/Page/Sponsors/Sponsor/index.jsx
@@ -5,8 +5,11 @@ import * as style from './style.css';
 
 const Sponsor = (props) => (
   <Panel className='panel-sponsor'>
-    <Image src={props.picture} responsive alt='member icon' />
-    <h4>{props.name}</h4>
+    <div id='sponsor-img-container' className='sponsor-img-container red-background'>
+      <Image className='sponsor-img' src={props.picture} responsive alt='sponsor picture' />
+    </div>
+    <h4 id='sponsor-name' className='orange-background'>{props.name}</h4>
+    <h5 id='sponsor-role' className='sponsor-role orange-background'>??</h5>
   </Panel>
 );
 
