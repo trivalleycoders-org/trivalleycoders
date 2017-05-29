615f4d727379a52ac2e04b94f4647c52897d83d3
klequis <klequis@gmail.com>
2017-05-29 08:33:15 -0700

    touching-up memebers styling

--- client/src/components/Page/Members/Member/index.jsx
+++ client/src/components/Page/Members/Member/index.jsx
@@ -4,14 +4,14 @@ import { Panel, Image } from 'react-bootstrap'
 import * as style from './style.css';
 
 const Member = (props) => (
-  <Panel id='member-panel' className='panel-member blue-background'>
+  <div id='member-panel' className='panel-member blue-background'>
     {/* className='img-sizing' */}
-    <div id='img-container' className='img-container red-background'>
+    <div id='img-container' className='panel-member-img-container red-background'>
       <Image src={props.picture} responsive alt='member icon' />
     </div>
     <h4 id='member-name' className='orange-background'>{props.name}</h4>
     <h5 id='member-role' className='orange-background'>{props.role}</h5>
-  </Panel>
+  </div>
 );
 
 export default Member;
--- client/src/components/Page/Members/index.jsx
+++ client/src/components/Page/Members/index.jsx
@@ -29,7 +29,7 @@ class Members extends Component {
               <Row className='main-row'>
                 <h1 className='section-title'>Members</h1>
                 <div className='members'>
-                  {this.props.members.map((m) => (
+                  {this.props.members.sort((a, b) => a.index - b.index).map((m) => (
                     <Member
                       key={m._id}
                       picture={m.picture}
