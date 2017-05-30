25e8c28b97d7775a32526105bf484cbc1d409689
klequis <klequis@gmail.com>
2017-05-30 13:12:23 -0700

    question

--- client/src/components/Page/Sponsors/Sponsor/index.jsx
+++ client/src/components/Page/Sponsors/Sponsor/index.jsx
@@ -5,11 +5,11 @@ import './style.css';
 
 const Sponsor = (props) => (
   <Panel className='panel-sponsor'>
-    <div id='sponsor-img-container' className='sponsor-img-container red-background'>
+    <div id='sponsor-img-container' className='sponsor-img-container'>
       <Image className='sponsor-img' src={props.picture} responsive alt='sponsor picture' />
     </div>
-    <h4 id='sponsor-name' className='orange-background'>{props.name}</h4>
-    <h5 id='sponsor-role' className='sponsor-role orange-background'>??</h5>
+    <h4 id='sponsor-name' className=''>{props.name}</h4>
+    <h5 id='sponsor-role' className='sponsor-role'>??</h5>
   </Panel>
 );
 
