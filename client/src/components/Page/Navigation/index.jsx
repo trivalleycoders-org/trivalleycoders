df16ec711b4e55dc1d83f59cac7bb5ebff6488b6
klequis <klequis@gmail.com>
2017-06-16 10:07:24 -0700

    nav basic functionality working

--- client/src/components/Page/Navigation/index.jsx
+++ client/src/components/Page/Navigation/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
+import classNames from 'classnames'
 import * as actionCreators from '../../../store/actions'
 import * as selectors from '../../../store/selectors'
 // import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
@@ -11,28 +12,46 @@ import './style.css';
 
 
 class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false,
+    }
+  }
   componentWillMount() {
     this.props.requestReadNavButtons();
   }
 
   render() {
+
+    let cx = classNames({
+      topnav: true,
+      responsive: this.state.showMenu
+    })
+
+    const handleIconClick = () => {
+      let val;
+      this.state.showMenu
+        ? val = false
+        : val = true
+      this.setState({showMenu: val})
+    }
     const { readNavButtonsRequest } = this.props;
     switch (readNavButtonsRequest.status){
       case 'success':
         return (
-            <Navbar id='navbar' inverse collapseOnSelect>
-              <Navbar.Collapse id='navbar-collapse'>
-                <Nav id='nav-pullRight' pullRight>
-                  <NavItem eventKey={1} href="#header">Home</NavItem>
-                  <NavItem eventKey={2} href="#tech-logos">Learn</NavItem>
-                  <NavItem eventKey={3} href="#events">Events</NavItem>
-                  <NavItem eventKey={4} href="#projects">Projects</NavItem>
-                  <NavItem eventKey={5} href="#members">Members</NavItem>
-                  <NavItem eventKey={6} href="#sponsors">Sponsors</NavItem>
-                </Nav>
-                <Navbar.Toggle />
-              </Navbar.Collapse>
-            </Navbar>
+          <div className={cx} id="myTopnav">
+            <a href="#home">Home</a>
+            <a href="#learn">Learn</a>
+            <a href="#events">Events</a>
+            <a href="#projects">Projects</a>
+            <a href="#members">Members</a>
+            <a href="#sponsors">Sponsors</a>
+            <a
+              className="icon"
+              onClick={() => handleIconClick()}
+            >&#9776;</a>
+          </div>
         )
       case 'failure':
        return (
