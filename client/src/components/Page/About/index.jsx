31a5cc99683de007af03f267fa1b6e9c87b50983
klequis <klequis@gmail.com>
2017-06-18 17:26:27 -0700

    formatted join us button

--- client/src/components/Page/About/index.jsx
+++ client/src/components/Page/About/index.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Grid, Row, Button} from 'react-bootstrap';
 import './style.css';
+import iMeetup from './../../../images/meetup-swarm.svg'
 
 const About = () => (
   <section id='about' className='section-about'>
@@ -10,7 +11,8 @@ const About = () => (
         <h1 className='section-title'>A place to learn, collaborate and<br />become a software developer</h1>
         <p className='lead'>Tri-Valley Coders is a comfortable non-competitive place to learn about making software, both in person and online, for beginners and vetrans alike. Our members span the gammute in terms of age and experience, but they all have one thing in common, they are learning something new and looking to take their software development skills to the next level. (or: ... and preparing to take the next step in their career.)</p>
         <p className='lead'>Tri-Valley Coders meets every Thursday from 9:00am to 12:00pm with frequent Saturday morning meetings as well. Click below to join.</p>
-        <Button>Join Us!</Button>
+        <Button className='join-us-btn' bsStyle='success' bsSize='large'> <img src={iMeetup} width={50}/><span className='join-us-text'></span>Join Us via Meetup!</Button>
+
       </Row>
     </Grid>
   </section>
