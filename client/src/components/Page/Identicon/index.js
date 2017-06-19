e3e1ce23d1a979a9df79bcf2fe83a9bce5fffcaf
klequis <klequis@gmail.com>
2017-06-18 17:34:12 -0700

    'initial commit'

---
+++ client/src/components/Page/Identicon/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Image } from 'react-bootstrap'
+import IdenticonJs from 'identicon.js';
+import * as ku from '../../../lib/ke-utils'
+
+const getRandomNumber = (min, max) => {
+  return parseInt(Math.random() * (max - min) + min);
+}
+const randomColorValue = (min=100, max=255) => {
+  const ret = getRandomNumber(min, max)
+  ku.log('randomColorValue', ret, 'blue')
+  return ret
+}
+
+const generateIdenticon = (hash) => {
+  // const data = new Identicon('d3b07384d113edec49eaa6238ad5ff00', 148).toString();
+  const options = {
+    foreground: [
+      randomColorValue(),
+      randomColorValue(0, 100),
+      randomColorValue(),
+      randomColorValue()
+    ], // rgba black
+    background: [255, 255, 255, 20], // rgba white
+    margin: 0.1, // 20% margin
+    size: 148, // 420px square
+    format: 'png', // use SVG instead of PNG
+  };
+  // const data = new Identicon(props._id, 148).toString();
+  const data = new IdenticonJs(hash, options).toString();
+  // const data = new Identicon('c157a79031e1c40f85931829bc5fc552', options).toString();
+  const src = `data:image/png;base64,${data}`
+  return <Image src={src} responsive alt='member picture' />
+}
+
+const Identicon = (props) => {
+  return generateIdenticon(props.hash)
+}
+
+export default Identicon
