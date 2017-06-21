d4c43467c2c89ae95c9ec74658950330af9d7ad4
klequis <klequis@gmail.com>
2017-06-21 13:42:38 -0700

    put alternated ui up when meetup api fails

--- client/src/components/Page/Events/index.jsx
+++ client/src/components/Page/Events/index.jsx
@@ -5,8 +5,9 @@ import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import * as actionCreators from '../../../store/actions';
 import * as selectors from '../../../store/selectors';
-import { Grid, Row } from 'react-bootstrap';
+import { Grid, Row, Button } from 'react-bootstrap';
 import Event from './Event';
+import iMeetup from './../../../images/meetup-swarm.svg'
 import './style.css';
 
 class Events extends Component {
@@ -46,17 +47,31 @@ class Events extends Component {
 
       case 'failure':
         return (
-          <section id='events'>
+          <section id='events'  className='events-section black-background'>
             <Grid className='outer-grid'>
               <Row className='main-row'>
-                <h2>Attempt to get events failed</h2>
+                <h1 className='section-title'>Events</h1>
+                <div className='events-failed-message lead'>
+                  <h2>We meet every Thursday!</h2>
+                  <ul className='ul-meeting-schedule'>
+                    <li>9:00 AM - Social time, Specialty's Cafe & Bakery, 5331 Hopyard Rd, Pleasanton</li>
+                    <li>9:45 AM - Move to Dublin Library, 200 Civic Plaza, Dublin</li>
+                    <li>10 AM - 12 PM - Code, get help and/or presentation</li>
+                  </ul>
+                  {/* <Button className='meeting-schedule' bsStyle='success' bsSize='large'> <img src={iMeetup} width={40} alt='meetup logo'/><span className='meeting-schedule-text'></span><a target='_blank' href='https://www.meetup.com/trivalleycoders/?scroll=true'>Tri-Valley Coders Meeting Schedule</a></Button> */}
+                  <Button className='meeting-schedule' href='https://www.meetup.com/trivalleycoders/?scroll=true' bsStyle='success' bsSize='large'>
+                    <img src={iMeetup} width={40} alt='meetup logo'/>
+                    Tri-Valley Coders Meeting Schedule
+                  </Button>
+
+                </div>
               </Row>
             </Grid>
           </section>
         );
       default:
         return (
-          <section id='events'>
+          <section id='events'  className='events-section black-background'>
             <Grid className='outer-grid'>
               <Row className='main-row'>
                 <h2>Loading data...</h2>
