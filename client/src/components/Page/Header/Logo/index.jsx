39a6db354a4785af38bb020b39fef242bdd18265
klequis <klequis@gmail.com>
2017-06-30 22:36:23 -0700

    mobile formatting

--- client/src/components/Page/Header/Logo/index.jsx
+++ client/src/components/Page/Header/Logo/index.jsx
@@ -1,10 +1,11 @@
 // Logo
 import React from 'react';
 import { Image } from 'react-bootstrap';
+import iLogo from '../../../../images/tvc.05.green.02.svg'
 import './style.css';
 
 const Logo = () => (
-  <Image id='tvc-logo' src='../../../../images/tvc.05.green.02.svg' responsive alt="logo" className='green-border tvc-logo'/>
+  <Image id='tvc-logo' src={iLogo} responsive alt="logo" className='green-border tvc-logo'/>
 );
 
 export default Logo;
