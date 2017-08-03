ac29a9bc0064b3d31c22127bb09676ce9a6ee493
klequis <klequis@gmail.com>
2017-08-03 16:14:06 -0700

    changed about text

--- src/components/Page/About/index.jsx
+++ src/components/Page/About/index.jsx
@@ -9,8 +9,9 @@ const About = () => (
     <div className='section-container container-fluid about-container'>
       <div className='about-sub-container eight-columns'>
         <p className='about-subtitle green-text'>A place to learn, collaborate and become a software developer</p>
-        <p>Tri-Valley Coders is a comfortable non-competitive place to learn about making software, both in person and online. We cater to beginners and vetrans alike. Our members span the gammute in terms of age and experience, but they all have one thing in common, they are learning something new, taking their software development skills to the next level and getting ready for the next step in their career.</p>
-        <p>Tri-Valley Coders meets every Thursday from 9:00am to 12:00pm with emphasis on Web development and the MERN Stack. We also have periodic meetings focused on Python and Data Science. Check our schedule below for dates and times.</p>
+        <p>Tri-Valley Coders is a place to learn the full gaumet of software development skills including: writing code, using developer tools and collaborating on projects. We offer the not just learning experiences but the opportunity to work on actual projects for real clients.</p>
+        <p>We have a collaborative and supportive structure with roles for beginners as well as experienced devlopers to learn new skills. Both our members and organizers bring a wide variety of experience and skill levels.</p>
+        <p>We meet every Thursday from 9:00am to 12:00pm with emphasis on Web development using JavaScript, HTML, CSS and the MERN Stack. We also have periodic meetings focused on Python and Data Science. Check our schedule below for dates and times.</p>
         <Button className='join-us-button' bsStyle='success'>
           <img className='join-meetup-logo' src={iMeetup} width={45} alt='meetup logo'/>
           <span className='join-us-text'>Join Us via Meetup!</span>
