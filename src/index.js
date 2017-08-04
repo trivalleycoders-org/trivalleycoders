0c5d6ba7dce18be3427907ede631bb703ec34029
klequis <klequis@gmail.com>
2017-08-03 20:00:11 -0700

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+import './bootstrap/css/bootstrap.css'
+
 import './index.css';
 import Page from './components/Page';
 
