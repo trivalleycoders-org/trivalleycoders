ae18ca0d018bb733f665d7ac86f862587702f1d8
klequis <klequis@gmail.com>
2018-09-22 08:51:48 -0700

    added digitalocean as sponsor

--- src/components/Page/Sponsors/Sponsor/index.jsx
+++ src/components/Page/Sponsors/Sponsor/index.jsx
@@ -2,16 +2,31 @@
 import React from 'react';
 import styles from './style.css';
 
-const Sponsor = (props) => {
-  const imgStyles = {
-    maxHeight: 72,
+const imgStyles = ({ maxWidth, maxHeight }) => {
+  console.log('maxWidth', maxWidth)
+  const base = {
+    maxHeight: maxHeight ? maxHeight : 72,
     margin: 'auto',
     marginBottom: 10,
   }
+  const width = {
+    maxWidth: maxWidth
+  }
+
+  return { ...base, ...width }
+
+}
+
+const Sponsor = (props) => {
+  // const imgStyles = {
+  //   maxHeight: 72,
+  //   margin: 'auto',
+  //   marginBottom: 10,
+  // }
   return (
     <div className={styles.panelSponsor}>
       <a href={props.url}>
-        <img style={imgStyles} src={props.picture} alt='sponsor' />
+        <img style={imgStyles(props)} src={props.picture} alt='sponsor' />
       </a>
 
       <h4 id='sponsor-name' className={styles.sponsorName}>{props.name}</h4>
--- src/components/Page/Sponsors/index.jsx
+++ src/components/Page/Sponsors/index.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import iZelda from './images/zelda2.jpg'
 import iBRIIA from './images/briia-logo.01.png'
+import iDO from './images/do-horizontal.png'
 import Sponsor from './Sponsor';
 import styles from './style.css';
 import SectionWrapper from '../../elements/SectionWrapper'
@@ -11,20 +12,29 @@ const Sponsors = () => {
 
     const arrSponsors = [
       {
-        id: 100,
-        picture: iZelda,
-        name: 'Zelda Kohn',
-        tagLine: 'For all your real estate needs in the Tri-valley!',
-        url: 'http://eastbayhomes.net'
-      },
-      {
         id: 200,
         picture: iBRIIA,
         name: 'BRIIA',
         tagLine: 'Bishop Ranch Intelligence Innovation Accelerator',
         url: 'https://briia.io/'
         // maxWidth: 10,
-      }
+      },
+      {
+        id: 300,
+        picture: iDO,
+        name: '',
+        tagLine: '',
+        url: 'https://www.digitalocean.com/',
+        maxWidth: 200,
+
+      },
+      {
+        id: 100,
+        picture: iZelda,
+        name: 'Zelda Kohn',
+        tagLine: 'For all your real estate needs in the Tri-valley!',
+        url: 'http://eastbayhomes.net'
+      },
     ]
 
     const sponsors = arrSponsors.map((s) => (
@@ -33,7 +43,7 @@ const Sponsors = () => {
         picture={s.picture}
         name={s.name}
         tagLine={s.tagLine}
-        width={s.maxWidth}
+        maxWidth={s.maxWidth}
         height={s.maxHeight}
         url={s.url}
       />
