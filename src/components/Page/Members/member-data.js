f7a3ec24796449f968139b5fe600bbf26a0a4ce0
klequis <klequis@gmail.com>
2018-09-22 08:32:16 -0700

    member changes

--- src/components/Page/Members/member-data.js
+++ src/components/Page/Members/member-data.js
@@ -3,6 +3,7 @@ import pMichael from './images/michael.jpg';
 import pNathan from './images/nathan.jpg';
 import pKevin from './images/kevin.jpg';
 import pCarl from './images/carl.jpg'
+import pHema from './images/hema.jpg'
 
 export const members = [
   {
@@ -13,9 +14,9 @@ export const members = [
     url: 'https://www.linkedin.com/in/carlbecker/'
   },
   {
-    id: 103,
-    picture: pLaura,
-    name: 'Laura',
+    id: 110,
+    picture: pHema,
+    name: 'Hema',
     role: 'Co-organizer Web Developer',
     url: 'https://www.linkedin.com/in/laura-silvey-29a6451a/'
   },
@@ -41,10 +42,11 @@ export const members = [
     url: 'https://www.linkedin.com/in/kevinsmithwebdev/'
   },
   {
-    id: '7a8a0c654b89fasdfghbe1b68adf9dbecc4f1db0',
-    picture: '',
-    name: 'Jim',
-    role: 'Web Developer',
+    id: 103,
+    picture: pLaura,
+    name: 'Laura',
+    role: 'Co-organizer Web Developer',
+    url: 'https://www.linkedin.com/in/laura-silvey-29a6451a/'
   },
 ]
 
